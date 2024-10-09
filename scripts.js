df6edc4ebfa3be59771817dc1c62677c35c54647
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2024-10-09 09:57:40 -0300

    02. Assíncronas, problemas ainda

--- scripts.js
+++ scripts.js
@@ -4,3 +4,33 @@ const upload = document.getElementById('upload');
 upload_btn.addEventListener('click', () => {
     upload.click();
 })
+
+function readFile(file){
+    return new Promise( (resolve, reject) =>{
+        const reader = new FileReader();
+        reader.onload = () => {
+            resolve({url: reader.result, name: file.name})
+        }
+        reader.onerror = () => {
+            reject(`Erro na leitura do arquivo ${file.name}`)
+        }
+        reader.readAsDataURL(file)
+    })
+}
+
+const main_img = document.querySelector('.main_img');
+const img_name = document.querySelector('.img_name_container p');
+
+upload.addEventListener('change', async (event) => {
+    const file = event.target.file[0];
+
+    if (file) {
+        try {
+            const contentFile = await readFile(file);
+            main_img.src = contentFile.url;
+            img_name.textContent = contentFile.name;
+        } catch (erro) {
+            console.error('Erro da leitura do arquivo')
+        }
+    }
+})
